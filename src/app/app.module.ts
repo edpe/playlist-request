c20d53769fcd758eb5ddf0f8c173a3d59cc01906
edpe <ed@edperkins.co.uk>
2022-01-08 14:53:45 +0000

    feat: column layout for providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProvidersComponent } from './providers/providers.component';
 
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent, ProvidersComponent],
@@ -15,6 +16,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCheckboxModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
