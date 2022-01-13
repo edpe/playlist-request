a3f348788f54adcc207afb0bc103bebd9988d143
edpe <ed@edperkins.co.uk>
2022-01-13 08:50:21 +0000

    feat: add basic html form elements

--- src/app/playlist-request-form/playlist-request-form.component.ts
+++ src/app/playlist-request-form/playlist-request-form.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-playlist-request-form',
   templateUrl: './playlist-request-form.component.html',
-  styleUrls: ['./playlist-request-form.component.css']
+  styleUrls: ['./playlist-request-form.component.css'],
 })
 export class PlaylistRequestFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
